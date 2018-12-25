c115b327535bad2253f926930f5aeb0f4bd0db20
KeYuan <keyuan258@gmail.com>
2018-12-25 23:07:41 +0800

    不再需要再单独导入执行脚本

--- bin/install-lose-jar.bat
+++
@@ -1,5 +0,0 @@
-echo [信息] 安装中央仓库缺失jar。如执行不成功，可以把下面的语句分条执行。
-call mvn install:install-file -Dfile=../lib/QRCode.jar -DgroupId=net.qrcode -DartifactId=qrcode -Dversion=1.0 -Dpackaging=jar
-call mvn install:install-file -Dfile=../lib/spssw-1.66.jar -DgroupId=net.spssw -DartifactId=spssw -Dversion=1.66 -Dpackaging=jar
-call mvn install:install-file -Dfile=../lib/xssProtect-0.1.jar -DgroupId=net.xssprotect -DartifactId=xssprotest -Dversion=1.0 -Dpackaging=jar
-call mvn install:install-file -Dfile=../lib/ueditor-1.1.2.jar -DgroupId=com.baidu -DartifactId=ueditor -Dversion=1.1.2 -Dpackaging=jar
--- bin/install-lose-jar.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-#可直接执行
-echo [信息] 安装中央仓库缺失jar。
-mvn install:install-file -Dfile=../lib/QRCode.jar -DgroupId=net.qrcode -DartifactId=qrcode -Dversion=1.0 -Dpackaging=jar
-mvn install:install-file -Dfile=../lib/spssw-1.66.jar -DgroupId=net.spssw -DartifactId=spssw -Dversion=1.66 -Dpackaging=jar
-mvn install:install-file -Dfile=../lib/xssProtect-0.1.jar -DgroupId=net.xssprotect -DartifactId=xssprotest -Dversion=1.0 -Dpackaging=jar
-mvn install:install-file -Dfile=../lib/ueditor-1.1.2.jar -DgroupId=com.baidu -DartifactId=ueditor -Dversion=1.1.2 -Dpackaging=jar
