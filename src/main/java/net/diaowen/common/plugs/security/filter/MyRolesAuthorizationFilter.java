cb18b03bdd9574c919d6588a1efbb1c91ccc39cf
wkeyuan <keyuan258@gmail.com>
2021-12-30 18:09:01 +0800

    清理没有使用的类引用

--- src/main/java/net/diaowen/common/plugs/security/filter/MyRolesAuthorizationFilter.java
+++ src/main/java/net/diaowen/common/plugs/security/filter/MyRolesAuthorizationFilter.java
@@ -1,9 +1,5 @@
 package net.diaowen.common.plugs.security.filter;
 
-import javafx.scene.SubScene;
-import net.diaowen.common.plugs.httpclient.HttpResult;
-import net.diaowen.common.plugs.httpclient.HttpStatus;
-import net.sf.json.JSONObject;
 import org.apache.shiro.subject.Subject;
 import org.apache.shiro.util.StringUtils;
 import org.apache.shiro.web.filter.authz.RolesAuthorizationFilter;
@@ -12,8 +8,6 @@ import org.apache.shiro.web.util.WebUtils;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
-
 
 public class MyRolesAuthorizationFilter extends RolesAuthorizationFilter {
 
