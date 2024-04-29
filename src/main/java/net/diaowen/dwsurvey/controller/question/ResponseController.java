a6870817c827001236903d6c8492cacfaf36eed9
CodeSky <service@diaowen.net>
2024-04-29 09:17:59 +0800

    更新二维码地址

--- src/main/java/net/diaowen/dwsurvey/controller/question/ResponseController.java
+++ src/main/java/net/diaowen/dwsurvey/controller/question/ResponseController.java
@@ -502,7 +502,7 @@ public class ResponseController {
 			encoderContent = baseUrl+"/static/diaowen/answer-m.html?sid="+sid;
 		}
 		if ("v6".equals(version)) {
-			encoderContent = baseUrl + "/#/v6/diaowen/"+sid;
+			encoderContent = baseUrl + "/#/v6/diaowen/an/"+sid;
 		}
 		if(StringUtils.isNotEmpty(ruleCode)){
 			encoderContent+="&ruleCode="+ruleCode;
