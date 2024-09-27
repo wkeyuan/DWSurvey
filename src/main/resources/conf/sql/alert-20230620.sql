e3714a9c2b07ee38a2ff19fc6d9465d76acb2687
CodeSky <service@diaowen.net>
2024-09-27 10:50:27 +0800

    加上需要修改的sql

--- src/main/resources/conf/sql/alert-20230620.sql
+++ src/main/resources/conf/sql/alert-20230620.sql
@@ -3,3 +3,12 @@ ALTER TABLE `dwsurvey`.`t_survey_json`
 
 ALTER TABLE `dwsurvey`.`t_survey_directory`
     CHANGE COLUMN `survey_type` `survey_type` VARCHAR(55) NULL DEFAULT NULL ;
+
+ALTER TABLE `dwsurvey`.`t_cascade_json`
+    CHANGE COLUMN `json_data` `json_data` JSON NULL DEFAULT NULL ;
+ALTER TABLE `dwsurvey`.`t_survey_json`
+    CHANGE COLUMN `survey_json_text` `survey_json_text` MEDIUMTEXT NULL DEFAULT NULL;
+
+ALTER TABLE `t_survey_directory`.`t_survey_json` CHANGE COLUMN `survey_type` `survey_type` VARCHAR(55) NULL DEFAULT NULL;
+ALTER TABLE `dwsurvey`.`t_survey_json`  ADD INDEX `surveyId` (`survey_id` ASC);
+ALTER TABLE `dwsurvey`.`t_survey_json`  ADD INDEX `sid` (`sid` ASC);
