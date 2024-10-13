adb02877cbf59aea1a1a1fa9bd2c5f1a20a46c3e
CodeSky <service@diaowen.net>
2024-10-13 20:53:42 +0800

    加入答卷地区信息

--- src/main/java/net/diaowen/dwsurvey/entity/es/answer/extend/EsAnIp.java
+++ src/main/java/net/diaowen/dwsurvey/entity/es/answer/extend/EsAnIp.java
@@ -3,10 +3,14 @@ package net.diaowen.dwsurvey.entity.es.answer.extend;
 public class EsAnIp {
 
     private String ip;
-    //省
+    //省级
     private String province;
-    //IP定位城市
+    //市级
     private String city;
+    //县级
+    private String county;
+    //乡级
+    private String town;
     //IP定位详细地址
     private String addr;
 
@@ -40,5 +44,21 @@ public class EsAnIp {
 
     public void setProvince(String province) {
         this.province = province;
+    }
+
+    public String getCounty() {
+        return county;
+    }
+
+    public void setCounty(String county) {
+        this.county = county;
+    }
+
+    public String getTown() {
+        return town;
+    }
+
+    public void setTown(String town) {
+        this.town = town;
     }
 }
